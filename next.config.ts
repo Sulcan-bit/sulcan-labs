1b2557b741da757cd6f44a4c0cabceb7ff7478c8
Sulcan-bit <rgsully@hotmail.com>
2026-09-19 09:02:40 -0600

    Add the Sulcan AI Chat Interface Rev11

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
-import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  experimental: {}
+  experimental: {},
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
-
 export default nextConfig;
+
 
